85bbb2c65a31506ab907ce2f0b6de19ba9962c66
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-04-23 15:10:11 +0800

    Added some comments in the program

--- src/Employee_FullTime.java
+++ src/Employee_FullTime.java
@@ -1,11 +1,15 @@
 public class Employee_FullTime extends Employee{
+
+    // Initialization - Variable declaration
     double monthlySalary;
 
+    // Constructor: Employee (Full time)
     Employee_FullTime(String name, int id, double monthlySalary){
         super(name, id);
         this.monthlySalary = monthlySalary;
     }
 
+    //Method: Override from Parent (Employee) - Calculate Salary
     @Override
     double calculateSalary(){
         return monthlySalary;
--- src/Employee_PartTime.java
+++ src/Employee_PartTime.java
@@ -1,14 +1,17 @@
 public class Employee_PartTime extends Employee {
 
+    // Initialization - Variable declaration
     double hourlyRate;
     double hoursWorked;
 
+    // Constructor: Employee (Part time)
     Employee_PartTime(String name, int id, double hourlyRate, double hoursWorked){
         super(name, id);
         this.hourlyRate = hourlyRate;
         this.hoursWorked = hoursWorked;
     }
 
+    //Method: Override from Parent (Employee) - Calculate Salary
     @Override
     double calculateSalary(){
         return hoursWorked * hourlyRate;
